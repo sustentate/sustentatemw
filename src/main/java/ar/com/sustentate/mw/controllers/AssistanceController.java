9c8ec6a620c3e083254641834772747d4250fbbd
Pablo Cavallo <pablo.cavallo@joinmosaic.com>
2018-11-13 09:34:30 -0800

    Fix url base for chatbot

--- src/main/java/ar/com/sustentate/mw/controllers/AssistanceController.java
+++ src/main/java/ar/com/sustentate/mw/controllers/AssistanceController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class AssistanceController {
 
-    @RequestMapping(value = "chat", method = RequestMethod.POST)
+    @RequestMapping(value = "/chat", method = RequestMethod.POST)
     public @ResponseBody AssistanceResponse chat(@RequestBody AssistanceRequest request) {
         AssistanceResponse r = new AssistanceResponse();
         r.setSentence(request.getSentence());
